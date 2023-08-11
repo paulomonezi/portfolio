005501212d35913069f1104734d5fbcb82926f38
Paulo Monezi <paulo.monezi@icloud.com>
2023-08-11 13:34:57 -0400

    feat(theme): added theme and toggler button

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -15,6 +15,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -73,6 +74,7 @@ const Navbar = props => {
           </LinkItem>
         </Stack>
         <Box flex={1} align="right">
+          <ThemeToggleButton />
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
@@ -82,10 +84,18 @@ const Navbar = props => {
                 aria-label="Options"
               />
               <MenuList>
-                  <MenuItem as={Link} href="/posts">About</MenuItem>
-                  <MenuItem as={Link} href="/works">Works</MenuItem>
-                  <MenuItem as={Link} href="/posts">Posts</MenuItem>
-                  <MenuItem as={Link} href='/'>View Source</MenuItem>
+                <MenuItem as={Link} href="/posts">
+                  About
+                </MenuItem>
+                <MenuItem as={Link} href="/works">
+                  Works
+                </MenuItem>
+                <MenuItem as={Link} href="/posts">
+                  Posts
+                </MenuItem>
+                <MenuItem as={Link} href="/">
+                  View Source
+                </MenuItem>
               </MenuList>
             </Menu>
           </Box>
